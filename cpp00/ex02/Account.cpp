3d55138c0f1ebab59a93038e246ab50770e68dec
“aldubar” <aldubar@student.42.fr>
2021-08-22 18:53:41 +0200

    cpp00 done

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: aldubar <aldubar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/22 16:53:18 by aldubar           #+#    #+#             */
-/*   Updated: 2021/08/22 18:47:58 by aldubar          ###   ########.fr       */
+/*   Updated: 2021/08/22 18:52:56 by aldubar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 #include <string>
 #include <ctime>
 #include <iostream>
+#include <iomanip>
 
 void	Account::_displayTimestamp( void) {
 
 	std::time_t	ts = std::time(nullptr);
 	tm			ts_struct = *std::localtime(&ts);
 	
-	std::cout << std::put_time(&ts_struct, "[%Y%m%d_%H%M%S] ";
+	std::cout << std::put_time(&ts_struct, "[%Y%m%d_%H%M%S] ");
 
 }
 
