25bc20aaa54abc366c8941f49288b33831031eea
“aldubar” <aldubar@student.42.fr>
2021-08-27 10:34:11 +0200

    remove std::string stoi cpp01 ex01

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -6,40 +6,19 @@
 /*   By: aldubar <aldubar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 10:27:42 by aldubar           #+#    #+#             */
-/*   Updated: 2021/08/26 23:05:45 by aldubar          ###   ########.fr       */
+/*   Updated: 2021/08/27 10:33:42 by aldubar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 #include <iostream>
 
-int		main( int ac, char **av ) {
+int		main( void ) {
 
-		int		N;
+		int					N = 10;
+		std::string const	name = "Heap zombie";
 
-		if (ac != 3) {
-
-			std::cout << "Usage: ./moarBrainz [nb of zombies] [name]" << std::endl;
-			return 1;
-
-		}
-		try {
-		
-			N = std::stoi( av[1] );
-		
-		} catch(std::invalid_argument) {
-
-			std::cout << "First argument must be a positive integer" << std::endl;
-			return 1;
-		}
-
-		if (N <= 0) {
-
-			std::cout << "First argument must be an integer greater than 0" << std::endl;
-			return 1;
-
-		}
-		Zombie*	horde = zombieHorde( N, av[2] );
+		Zombie*	horde = zombieHorde( N, name );
 
 		for (int i = 0; i < N; i++) {
 		
