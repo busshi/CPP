48b4ea2d4fb053b7d2555b3011d24fe6290dcf7b
aldubar <aldubar@student.42.fr>
2021-09-21 09:34:00 +0200

    fix animal err cpp04/ex02/main.cpp

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aldubar <aldubar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 15:24:26 by aldubar           #+#    #+#             */
-/*   Updated: 2021/09/20 16:41:30 by aldubar          ###   ########.fr       */
+/*   Updated: 2021/09/21 09:33:38 by aldubar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 int	main() {
 
 //	Uncomment the next line to verify that Animal is an abstract Class
-//	Animal		animal;
+//	Animal		animal_err;
 
 	Cat		cat;
 	Animal	*animal = &cat;
